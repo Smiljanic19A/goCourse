7bc81ed1d530a4c8109b276dc72e0b3a38d8acfa
Smiljanic19A <aleksandars@app-works.app>
2025-12-12 10:34:05 +0100

    -- Section 3 -- project 1 -- Creating deck refactor

--- cards_project/deck.go
+++ cards_project/deck.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-import "math/rand/v2"
 
 type deck []string
 
@@ -13,27 +12,16 @@ func (d deck) print() {
 }
 
 func newDeck() deck {
-	combinations := []string{
-		// Hearts
-		"AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH",
-		// Diamonds
-		"AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD",
-		// Clubs
-		"AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC",
-		// Spades
-		"AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS",
-	}
 
 	d := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 
-	i := 0
-
-	for i < len(combinations) {
-		randomInt := rand.IntN(len(combinations))
-		d = append(d, combinations[randomInt])
-		combinations = append(combinations[:randomInt], combinations[randomInt+1:]...)
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			d = append(d, value+" of "+suit)
+		}
 	}
 
 	return d
-
 }
