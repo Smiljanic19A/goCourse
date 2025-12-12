81c75cc15d69db2ab133b900b11f11b529844708
Smiljanic19A <aleksandars@app-works.app>
2025-12-12 10:35:47 +0100

    -- Section 3 -- project 1 -- Creating deck refactor

--- cards_project/main.go
+++ cards_project/main.go
@@ -1,22 +1,6 @@
 package main
 
-import "math/rand/v2"
-
 func main() {
 	cards := newDeck()
 	cards.print()
-}
-
-func determineCardValue() string {
-	return "Ace Of Spades"
-}
-
-func makeRandomCard() string {
-	randomInt := rand.IntN(3) + 1
-	if randomInt == 1 {
-		return "ACE OF SPADES"
-	} else if randomInt == 2 {
-		return "ACE OF DIAMONDS"
-	}
-	return "King Of Hearts"
 }
