c59060fa9ecfcd72008cc3ab3ef3f74ce95fc202
Smiljanic19A <aleksandars@app-works.app>
2025-12-18 16:09:23 +0100

    -- Section 3 -- project 1 -- Rewritting everythinbg in cards project to test knowledge

--- project_1_upgrade/deck.go
+++ project_1_upgrade/deck.go
@@ -1,1 +1,61 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type deck []string
+
+func newDeck() deck {
+	d := deck{}
+	suites := []string{"Spades", "Clubs", "Hearts", "Diamonds"}
+	values := []string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Ace", "Jack", "Queen", "King"}
+
+	for _, suit := range suites {
+		for _, value := range values {
+			d = append(d, value+" Of "+suit)
+		}
+	}
+
+	return d
+}
+
+func deal(d deck, handSize int) (deck, deck, error) {
+	n := len(d)
+	cardsToLeave := n - handSize
+	if cardsToLeave < 0 {
+		return deck{}, deck{}, errors.New("deck is too small to deal")
+	}
+
+	return d[:cardsToLeave], d[cardsToLeave:], nil
+}
+
+func fromFile(filename string) (deck, error) {
+	var d deck
+	deckBytes, err := os.ReadFile(filename)
+	if err != nil {
+		return deck{}, err
+	}
+
+	cardSlice := strings.Split(string(deckBytes), "\n")
+	for _, card := range cardSlice {
+		d = append(d, card)
+	}
+	return d, nil
+}
+
+func (d deck) print() {
+	fmt.Println(d.toString())
+}
+func (d deck) toString() string {
+	return strings.Join(d, "\n")
+}
+func (d deck) toBytes() []byte {
+	return []byte(d.toString())
+}
+func (d deck) toFile(filename string) error {
+	return os.WriteFile(filename, d.toBytes(), 0644)
+}
--- project_1_upgrade/main.go
+++ project_1_upgrade/main.go
@@ -1,7 +1,43 @@
 package main
 
+import "fmt"
+
 //requirements
-// create deck
+// create deck done
 // deal hand
 // write to file
 // read from file
+
+func main() {
+	d := newDeck()
+
+	remainingDeck, _, err := deal(d, 5)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		var hand deck
+
+		remainingDeck, hand, err = deal(remainingDeck, 5)
+
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		err = hand.toFile("hand.txt")
+		if err != nil {
+			fmt.Println("Error writing to file")
+			fmt.Println(err)
+		}
+		fmt.Println("New deck size", len(remainingDeck), "hand size", len(hand))
+	}
+
+	randomDeck, err := fromFile("hand.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	randomDeck.print()
+}
