16d4c824da385ebc0a03b36811e22430620afa11
DroidStudioAS <as.droiddevteam@gmail.com>
2025-12-20 23:54:42 +0100

    Section 4 Defining structs

--- project_2/main.go
+++ project_2/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+type person struct {
+	firstName string
+	lastName  string
+	age       int
+}
+
+func main() {
+	dude := person{
+		firstName: "Dude",
+		lastName:  "Mary",
+		age:       25,
+	}
+
+	fmt.Println(dude)
 }
