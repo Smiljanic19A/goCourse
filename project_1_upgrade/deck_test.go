d15046d9cc2af723cbfca2414d03d558556f1f62
DroidStudioAS <as.droiddevteam@gmail.com>
2025-12-20 18:23:43 +0100

    Section 3 testing Testing deck IO

--- project_1_upgrade/deck_test.go
+++ project_1_upgrade/deck_test.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"math/rand"
+	"os"
 	"testing"
 )
 
 const ExpectedNumberOfCards = 56
+const IoFileName = "DeckOfCards.txt"
 
+func cleanupAfterTests(t *testing.T) {
+
+	_, err := os.Stat(IoFileName)
+
+	if err == nil {
+		err := os.Remove(IoFileName)
+		if err != nil {
+			t.Errorf("Error removing %s file", IoFileName)
+		}
+	}
+}
 func TestNumberOfCards(t *testing.T) {
 	d := newDeck()
 	if len(d) != ExpectedNumberOfCards {
@@ -23,4 +37,31 @@ func TestFirstAndLastCard(t *testing.T) {
 	if d[len(d)-1] != "King Of Diamonds" {
 		t.Errorf("Expected last card but got %s", d[len(d)-1])
 	}
+}
+
+func TestDeckIo(t *testing.T) {
+	cleanupAfterTests(t)
+	d := newDeck()
+	d.shuffle(33)
+
+	err := d.toFile(IoFileName)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	ioDeck, err := fromFile(IoFileName)
+	if err != nil {
+		t.Errorf("%v", err)
+	}
+
+	for i := 0; i < 56; i++ {
+		randomInt := rand.Intn(len(d) - 1)
+
+		if d[randomInt] != ioDeck[randomInt] {
+			t.Errorf("Decks dont match")
+		}
+
+	}
+	cleanupAfterTests(t)
+
 }
