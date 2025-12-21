b7adc7e878f6daa55cf4b65e6907ba50907b3fa6
DroidStudioAS <as.droiddevteam@gmail.com>
2025-12-21 12:53:57 +0100

    -- Section 4 -- POINTERS START - Reference vs value types

--- project_2/main.go
+++ project_2/main.go
@@ -4,13 +4,20 @@ import "fmt"
 
 func main() {
 	someSlice := []string{"a", "b", "c"}
+	someString := "hello"
+	updateString(someString)
 	updateSlice(someSlice)
 
 	fmt.Println(someSlice)
+	fmt.Println(someString)
 }
 
 func updateSlice(slice []string) {
 	slice[0] = "hello"
+}
+
+func updateString(s string) {
+	s = "new"
 }
 
 // Why does this work - why did it not rqueire a pointer?/
