126a8381905138f8c6c43a4f5b6e6b43f04c890f
DroidStudioAS <as.droiddevteam@gmail.com>
2025-12-21 23:36:04 +0100

    -- Section 5 -- Maps -- initializing - different ways

--- maps/main.go
+++ maps/main.go
@@ -4,8 +4,8 @@ import "fmt"
 
 func main() {
 
-	var colors map[string]string
-
+	//var colors map[string]string
+	colors := make(map[string]string)
 	//colors := map[string]string{
 	//	"red":   "#ff0000",
 	//	"green": "#00ff00",
